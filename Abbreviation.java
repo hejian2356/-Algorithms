210618add1a7c831762279472b873915131728b6
jianh <jianh@hejiandeMacBook-Pro.local>
2015-10-27 22:29:27 -0700

    change to be easier

--- Abbreviation.java
+++ Abbreviation.java
@@ -4,72 +4,44 @@ class Abbreviation {
 	 * Test: given "sat", return [1a1, 3, 1at, 2t, sa1, s2, sat, s1t].       
 	 * 
 	 */
-	List<String> abbre(String s) {
-		List<String> res = new LinkedList<>();
-		if (s.length() == 0) {
-			res.add("");
-			return res;
-		}
-		if (s.length() == 1) {
-			res.add("1");
-			res.add(s);
-			return res;
-		}
-		int i = s.length() / 2;
-		List<String> l = null, r = null;
-		if (i == 0) {
-			l = abbre("");
-		} else {
-			l = abbre(s.substring(0, i));
-		}
-		if (i == s.length() - 1) {
-			r = abbre("");
-		} else {
-			r = abbre(s.substring(i + 1, s.length()));
-		}
-		for (int j = 0; j < l.size(); j++) {
-			for (int k = 0; k < r.size(); k++) {
-				String tmp = l.get(j) + s.charAt(i) + r.get(k);
-				res.add(tmp);
-				String left = l.get(j), right = r.get(k);
-				int i1;
-				for (i1 = left.length() - 1; i1 >= 0; i1--) {
-					char c = left.charAt(i1);
-					if (!(c >= '0' && c <= '9')) {
-						break;
-					}
-				}
-				int endLeft = i1;
-
-				for (i1 = 0; i1 < right.length(); i1++) {
-					char c = right.charAt(i1);
-					if (!(c >= '0' && c <= '9')) {
-						break;
-					}
-				}
-				int staRight = i1;
-				if (endLeft == left.length() - 1 && staRight == 0) {
-					res.add(left + "1" + right);
-				} else if (endLeft == left.length() - 1) {
-					int rightNum = Integer.parseInt(right
-							.substring(0, staRight));
-					res.add(left + ("" + (1 + rightNum))
-							+ right.substring(staRight));
-				} else if (staRight == 0) {
-					int leftNum = Integer.parseInt(left.substring(endLeft + 1));
-					res.add(left.substring(0, endLeft + 1)
-							+ ("" + (leftNum + 1)) + right);
-				} else {
-					int leftNum = Integer.parseInt(left.substring(endLeft + 1));
-					int rightNum = Integer.parseInt(right
-							.substring(0, staRight));
-					res.add(left.substring(0, endLeft + 1)
-							+ ("" + (leftNum + 1 + rightNum))
-							+ right.substring(staRight));
-				}
-			}
-		}
-		return res;
-	}
+    public List<String> abbre(String s) {
+        List<String> res = new ArrayList<>();
+        if (s.length() == 0) {
+            res.add("");
+            return res;
+        }
+        if (s.length() == 1) {
+            res.add(s);
+            res.add("1");
+            return res;
+        }
+        List<String> left = abbre(s.substring(0, s.length()/2)), right = abbre(s.substring(s.length()/2));
+        for (String a: left) {
+            for (String b: right) {
+                int pos1 = -1, num1 = -1, pos2 = -1, num2 = -1;
+                if (a.length() != 0 && Character.isDigit(a.charAt(a.length()-1))) {
+                    pos1 = a.length()-1;
+                    while (pos1 >= 0 && Character.isDigit(a.charAt(pos1))) {
+                        pos1--;
+                    }
+                    num1 = Integer.parseInt(a.substring(pos1+1));
+                }
+                if (b.length() != 0 && Character.isDigit(b.charAt(0))) {
+                    pos2 = 0;
+                    while (pos2 < b.length() && Character.isDigit(b.charAt(pos2))) {
+                        pos2++;
+                    }
+                    num2 = Integer.parseInt(b.substring(0, pos2));
+                }
+                if (num1 == -1 || num2 == -1) {
+                    res.add(a+b);
+                }
+                else {
+                    num1 += num2;
+                    res.add(a.substring(0, pos1+1)+num1+b.substring(pos2));
+                }
+            }
+        }
+        return res;
+    }
 }
-
