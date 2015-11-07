2947c20bd98d056c0aa81bfe32048d194bceeb0a
jianh <jianh@hejiandeMacBook-Pro.local>
2015-11-06 19:50:07 -0800

    use modified merge sort

--- CountBigElementNumberRightSideofCur.java
+++ CountBigElementNumberRightSideofCur.java
@@ -50,3 +50,58 @@ class Solution2 {
 		return res;
 	}
 }
+
+
+///////////count smaller numbers' number
+//surpass
+	//given {4, 3, 2, 1}, return {3, 2, 1, 0}
+	private void merge(int[] nums, int[] pos, int sta, int mid, int end, int[] res) {
+		int[] tmp = new int[nums.length];
+		for (int i = sta; i <= end; i++) {
+			tmp[i] = pos[i];
+		}
+		int l = sta, r = mid+1, idx = sta;
+		while (l <= mid && r <= end) {
+			if (nums[tmp[l]] <= nums[tmp[r]]) {
+				pos[idx] = tmp[l];
+				res[pos[idx]] += r-mid-1;
+				l++;
+				idx++;
+			}
+			else {
+				pos[idx++] = tmp[r++];
+			}
+		}
+		while (l <= mid) {
+			pos[idx] = tmp[l];
+			res[pos[idx]] += end-mid;
+			idx++;
+			l++;
+		}
+		while (r <= end) {
+			pos[idx++] = tmp[r++];
+		}
+	}
+	
+	private void mergeSort(int[] nums, int[] pos, int sta, int end, int[] res) {
+		if (sta >= end) {
+			return;
+		}
+		int mid = (sta + end) / 2;
+		mergeSort(nums, pos, sta, mid, res);
+		mergeSort(nums, pos, mid+1, end, res);
+		merge(nums, pos, sta, mid, end, res);
+	}
+	
+	public List<Integer> surpass(int[] nums) {
+		int[] res = new int[nums.length], pos = new int[nums.length];
+		for (int i = 0; i < pos.length; i++) {
+			pos[i] = i;
+		}
+		mergeSort(nums, pos, 0, nums.length-1, res);
+		List<Integer> result = new ArrayList<>();
+		for (int x: res) {
+			result.add(x);
+		}
+		return result;
+	}
